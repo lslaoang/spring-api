0579a2c8f591f565412b80623f8e688496c61823
Laurel Lao Ang <laurellaoang@gmail.com>
2021-10-23 11:29:31 +0800

    Ratings API created

---
+++ datings-data-service/src/main/java/io/lslaoang/datingsdataservice/resource/RatingsResource.java
@@ -0,0 +1,16 @@
+package io.lslaoang.datingsdataservice.resource;
+
+import io.lslaoang.datingsdataservice.model.Rating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId){
+        return  new Rating(movieId,4);
+    }
+}
