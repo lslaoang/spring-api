953f71e83e385c2746306abf2644a8aa42bca84c
Laurel Lao Ang <laurellaoang@gmail.com>
2021-10-23 11:45:57 +0800

    Hard coding Rating API

--- movie-catalog-service/src/main/java/io/lslaoang/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/lslaoang/moviecatalogservice/resource/MovieCatalogResource.java
@@ -1,12 +1,16 @@
 package io.lslaoang.moviecatalogservice.resource;
 
 import io.lslaoang.moviecatalogservice.model.CatalogItem;
+import io.lslaoang.moviecatalogservice.model.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -14,8 +18,15 @@ public class MovieCatalogResource {
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId")  String userId){
-        return Collections.singletonList(
-                new CatalogItem("Transformers","test",4)
+
+        //Get all ratings
+        List<Rating> ratings = Arrays.asList(
+                new Rating("123",4),
+                new Rating("456",5)
         );
+
+        return ratings.stream().map(rating -> new CatalogItem("Transformers","test",4))
+                .collect(Collectors.toList());
+
     }
 }
