22a6144f875f587a4da35611e7487fe33d6f565b
Laurel Lao Ang <laurellaoang@gmail.com>
2021-10-23 11:13:56 +0800

    setting controllers and model creation

---
+++ movie-info-service/src/main/java/io/lslaoang/movieinfoservice/resource/MovieResource.java
@@ -0,0 +1,16 @@
+package io.lslaoang.movieinfoservice.resource;
+
+import io.lslaoang.movieinfoservice.model.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovie(@PathVariable("movieId") String movieId){
+        return new Movie(movieId,"Test name");
+    }
+}
