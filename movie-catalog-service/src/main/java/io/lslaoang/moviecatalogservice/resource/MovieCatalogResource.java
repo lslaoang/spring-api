5e01f51e8bfa15bfc9395662a4e7695c98f54f9a
Laurel Lao Ang <laurellaoang@gmail.com>
2021-10-23 12:23:31 +0800

    Refactored Ratings API

--- movie-catalog-service/src/main/java/io/lslaoang/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/lslaoang/moviecatalogservice/resource/MovieCatalogResource.java
@@ -3,14 +3,13 @@ package io.lslaoang.moviecatalogservice.resource;
 import io.lslaoang.moviecatalogservice.model.CatalogItem;
 import io.lslaoang.moviecatalogservice.model.Movie;
 import io.lslaoang.moviecatalogservice.model.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,10 +17,11 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId")  String userId){
-
-        RestTemplate restTemplate = new RestTemplate();
 
         //Get all ratings
         List<Rating> ratings = Arrays.asList(
@@ -31,7 +31,7 @@ public class MovieCatalogResource {
 
         return ratings.stream().map(rating -> {
 
-            Movie movie = restTemplate.getForObject("http://localhost:8081/movies/"+rating.getMovieId(), Movie.class);
+            Movie movie = restTemplate.getForObject("http://localhost:8083/movies/"+rating.getMovieId(), Movie.class);
             return new CatalogItem(movie.getName(), "desc",rating.getRating());
 
         })
